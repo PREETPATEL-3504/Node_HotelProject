cb44ca12015ec604b617872082a2de4cc3c63c04
PREETPATEL-3504 <preetpatel3504@gmail.com>
2024-04-11 11:28:43 +0530

    Host on render

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 // const mongoURL = 'mongodb://localhost:27017/hotels'
 
-const mongoURL = 'mongodb+srv://preetpatel3504:preet3504@nodehotelcluster.nvjckqh.mongodb.net/'
+const mongoURL = process.env.DB_URL
 
 mongoose.connect(mongoURL)
 
